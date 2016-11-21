bec7a82b7448d56ddc29a3aa72b6ed57f07363e1
Dzhelil Ramadan <dzhelilramadan@gmail.com>
2016-11-21 13:40:22 +0200

    Initial commit

---
+++ src/main/java/model/IbanWrapper.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Created by DRamadan on 21-Nov-16.
+ */
+public class IbanWrapper {
+
+    private List<String> ibans;
+
+    public List<String> getIbans() {
+        return ibans;
+    }
+
+    public void setIbans(List<String> ibans) {
+        this.ibans = ibans;
+    }
+
+}
