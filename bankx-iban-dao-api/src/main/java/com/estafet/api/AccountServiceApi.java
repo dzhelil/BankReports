229126dbf438dff88524c61e389aa72cded4d697
Dzhelil Ramadan <dzhelilramadan@gmail.com>
2016-12-09 11:12:39 +0200

    Added persist methods to the service

--- bankx-iban-dao-api/src/main/java/com/estafet/api/AccountServiceApi.java
+++ bankx-iban-dao-api/src/main/java/com/estafet/api/AccountServiceApi.java
@@ -2,6 +2,7 @@ package com.estafet.api;
 
 
 import com.estafet.pojo.Account;
+import com.estafet.pojo.AccountDB;
 
 /**
  * Created by DRamadan on 24-Nov-16.
@@ -10,4 +11,7 @@ public interface AccountServiceApi {
 
     public abstract Account getAccountByIban(String iban);
 
+    public abstract void persistAccount(AccountDB account);
+
+    public abstract void mergeAccount(AccountDB account);
 }
